83bfc1476e7d7231a522493e8ba0010c8dcdc49c
Hígor Rafael Santos da Silva <166553851+HigorTk@users.noreply.github.com>
2024-04-26 17:04:42 -0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -1,22 +1,26 @@
+# Importando bibliotécas
 
 import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-
+# Configuração de Layout da página 
 st.set_page_config(layout="wide")
 
+# Abrindo dataset
 df_reviews = pd.read_csv("datasets/customer reviews.csv")
 df_top100_books = pd.read_csv("datasets/Top-100 Trending Books.csv")
 
-
+# Range para seleção de livros
 price_max = df_top100_books["book price"].max()
 price_min = df_top100_books["book price"].min()
 
+# Slider 
 max_price = st.sidebar.slider("Price Range", price_min, price_max, price_max)
 df_books = df_top100_books[df_top100_books["book price"] <= max_price]
 df_books
 
+# Graficos para análise visualização
 fig = px.bar(df_books["year of publication"].value_counts())
 fig2 = px.histogram(df_books["book price"])
 
